6fa6c0dcfa1edc6c3b71aa46a0d70e03c9e50841
slipkinem <398873901@qq.com>
2017-06-26 15:20:06 +0800

    init

---
+++ chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,30 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by slipkinem on 6/26/2017.
+ */
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        request.setAttribute("currentTime", currentTime);
+//        dispatcher设置转发路径
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp")
+//                确定转发
+                .forward(request, response);
+    }
+}
