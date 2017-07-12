1f40b06e046701666715463d592e4d5d77a9eb94
slipkinem <ls123456>
2017-07-12 21:45:47 +0800

    merge

--- smart4j/chapter/src/main/java/org/smart4j/chapter3/controller/TestControlelr.java
+++ smart4j/chapter/src/main/java/org/smart4j/chapter3/controller/TestControlelr.java
@@ -1,6 +1,8 @@
 package org.smart4j.chapter3.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.smart4j.framework.annotation.Action;
 import org.smart4j.framework.annotation.Controller;
 import org.smart4j.framework.bean.Param;
@@ -9,18 +11,25 @@ import org.smart4j.framework.bean.View;
 import java.util.Map;
 
 /**
+ * testController
  * Created by slipkinem on 7/3/2017.
  */
 
 @Controller
 public class TestControlelr {
+    private  static final Logger logger = LoggerFactory.getLogger(TestControlelr.class);
+
+    @Action("get:/")
+    public View index () {
+        return new View("index.jsp");
+    }
 
     @Action("get:/hello")
     public View test(Param param) {
-        System.out.println(param);
+        logger.info(String.valueOf(param.getParamMap()));
         Map<String, Object> paramMap = param.getParamMap();
 
-        View view = new View("index.jsp");
+        View view = new View("test.jsp");
         view.addModel("test", "hehe");
         for (String key : paramMap.keySet()) {
             view.addModel(key, paramMap.get(key));
---
+++ smart4j/chapter/src/main/webapp/WEB-INF/view/test.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: slipkinem
+  Date: 7/3/2017
+  Time: 3:36 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Title</title>
+</head>
+<body>
+ hello
+${test}
+${hello}
+ <button>ajax</button>
+</body>
+<script>
+  function save() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("post", "")
+  }
+</script>
+</html>
